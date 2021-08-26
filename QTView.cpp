9f1e6727f040ac1f6511f30d6495a378d10977da
ErliPan <github@erlipan.dev>
2021-08-27 01:52:46 +0200

    Move to QT

--- QTView.cpp
+++ QTView.cpp
@@ -2,4 +2,4 @@
 // Created by erli on 25/08/21.
 //
 
-#include "NCView.h"
+#include "QTView.h"
---
+++ QTView.h
@@ -0,0 +1,16 @@
+//
+// Created by erli on 25/08/21.
+//
+
+#ifndef NCNOTE_QTVIEW_H
+#define NCNOTE_QTVIEW_H
+
+
+#include "ViewInterface.h"
+
+class QTView : public ViewInterface {
+
+};
+
+
+#endif //NCNOTE_QTVIEW_H
