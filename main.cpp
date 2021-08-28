1558a1aefca29f2fdcf6d27f3eb34164dd270d9f
ErliPan <github@erlipan.dev>
2021-08-29 00:30:25 +0200

    Main remove hello world

--- main.cpp
+++ main.cpp
@@ -3,12 +3,8 @@
 #include "QTView.h"
 
 int main(int argc, char *argv[]) {
-    std::cout << "Hello, World!" << std::endl;
     QApplication a(argc, argv);
     Collections collection;
     QTView w(&collection);
-
-
-
     return a.exec();
 }
